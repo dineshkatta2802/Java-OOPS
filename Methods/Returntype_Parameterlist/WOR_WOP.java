d16517a556fe3502afb3ffe31ad3551de0182ca9
Dinesh Katta <kattadinesh2@gmail.com>
2024-05-19 13:57:10 +0530

    The name is changed for inside folder

---
+++ Methods/Returntype_Parameterlist/WOR_WOP.java
@@ -0,0 +1,20 @@
+// ⁡⁣⁣⁢Illustartion of Method Without Return Type and Without Parameter list⁡
+
+class sample {
+//Lets create an Instance Variable 
+double num1,num2;
+sample(double x,double y){
+    num1 = x;
+    num2 = y;
+}
+void sum(){
+    double result = num1 + num2;
+    System.out.println("Sum = "+result);
+}
+}
+public class WOR_WOP {
+
+    public static void main(String[] args) {
+        
+    }
+}
