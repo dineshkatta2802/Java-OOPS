03a3edcd8833ebbdb896e6f1692cf157391075b5
Dinesh Katta <kattadinesh2@gmail.com>
2024-05-24 14:07:54 +0530

    New files added

--- Methods/Method_Overloading.java
+++ Methods/Method_Overloading.java
@@ -1,7 +1,10 @@
 package Methods;
 //⁡⁣⁣⁢--->Note:⁡
-//A  class having 2 or maore methos with same name but different parameter list
+//A  class having 2 or more methods with same name but different parameter list
+//Also known as ⁡⁣⁢⁣Compile-time Polymorphism⁡
+
 class Overloading{
+    //Method Overloading with ⁡⁣⁢⁣different Parameteers⁡
     static int add(int a, int b){
         return a+b;
     }
@@ -10,13 +13,22 @@ class Overloading{
         return a+b+c;
     }
 
-    //Method Overloading with differnt datatypes
+    //Method Overloading with ⁡⁣⁢⁣differnt datatypes⁡
     static int mul(int a ,int b){
         return a*b;
     }
 
     static double mul(double a,double b, double c){
         return a*b*c;
+    }
+
+    //Method Overloading with different Order of parameters
+    static void Student_id(int a , String b){
+        System.out.println(a+" "+b); ;
+    }
+
+    static void Student_id(String  b ,int a){
+        System.out.println(b+" "+a);
     }
     
 }
@@ -27,5 +39,7 @@ public class Method_Overloading {
         System.out.println(x.add(10, 20, 30));
         System.out.println(x.mul(10, 20));
         System.out.println(x.mul(10.2, 20.1, 30.12));
+        System.out.println(x.Student_id(10, "Dinesh"));  
+        System.out.println(x.Student_id("Dinesh", 20););  
     }
 }
--- Methods/Method_Overriding.java
+++ Methods/Method_Overriding.java
@@ -10,18 +10,26 @@ package Methods;
 //where three classes are overriding the method of a parent class.  
 //Creating a parent class.  
 class Bank{  
-    int getRateOfInterest(){return 0;}  
+    int getRateOfInterest(){
+        return 0;
+    }  
 }  
 //Creating child classes.  
-class SBI extends Bank{  
-    int getRateOfInterest(){return 8;}  
-}  
-class ICICI extends Bank{  
-    int getRateOfInterest(){return 7;}  
-}  
-class AXIS extends Bank{  
-    int getRateOfInterest(){return 9;}  
-}  
+        class SBI extends Bank{  
+            int getRateOfInterest(){
+                return 8;
+            }  
+        }  
+        class ICICI extends Bank{  
+            int getRateOfInterest(){
+                return 7;
+            }  
+        }  
+        class AXIS extends Bank{  
+            int getRateOfInterest(){
+                return 9;
+            }  
+        }  
 
 //Test class to create objects and call the methods  
 public class Method_Overriding{  
