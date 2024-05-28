43b72b23a624c35f383cf3dd309b6e0e0a3e51ae
Dinesh Katta <kattadinesh2@gmail.com>
2024-05-28 12:36:55 +0530

    New changes are made

--- Methods/Method_Overriding.java
+++ Methods/Method_Overriding.java
@@ -1,45 +1,40 @@
-package Methods;
 //⁡⁣⁣⁢--->Note:⁡
-//If subclass (child class) has the same method as declared in the parent class,
-// it is known as method overriding in Java.
+//Method overriding is one of the ways by which Java achieves ⁡⁣⁢⁣Run Time Polymorphism⁡⁣⁢⁣(Dynamic Polymorphism)⁡⁡. 
+//Writing 2 or more methods in super and sub class such that the methods have same name and parameters.
 
-//In other words, If a subclass provides the specific implementation of the method that
-// has been declared by one of its parent class, it is known as method overriding.
 
-//Java Program to demonstrate the real scenario of Java Method Overriding  
-//where three classes are overriding the method of a parent class.  
-//Creating a parent class.  
-class Bank{  
-    int getRateOfInterest(){
-        return 0;
-    }  
-}  
-//Creating child classes.  
-        class SBI extends Bank{  
-            int getRateOfInterest(){
-                return 8;
-            }  
-        }  
-        class ICICI extends Bank{  
-            int getRateOfInterest(){
-                return 7;
-            }  
-        }  
-        class AXIS extends Bank{  
-            int getRateOfInterest(){
-                return 9;
-            }  
-        }  
+package Methods;
+// Base Class
+class Parent {
+	void show() { System.out.println("Parent's show()"); }
+}
 
-//Test class to create objects and call the methods  
-public class Method_Overriding{  
-    public static void main(String args[]){  
-    SBI s=new SBI();  
-    ICICI i=new ICICI();  
-    AXIS a=new AXIS();  
-    System.out.println("SBI Rate of Interest: "+s.getRateOfInterest());  
-    System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest());  
-    System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());  
-    }  
-    }  
-    
+// Inherited class
+class Child extends Parent {
+	// This method overrides show() of Parent
+	@Override void show()    //⁡⁣⁣⁢--->Note:⁡
+                             //@Override annotation is to inform the compiler and the 
+                             //reader that the method is intended to override a method 
+                             //in its superclass (Parent in this case)
+	{
+		System.out.println("Child's show()");
+	}
+}
+
+class Method_Overriding {
+	public static void main(String[] args)
+	{
+		// If a Parent type reference refers
+		// to a Parent object, then Parent's
+		// show is called
+		Parent obj1 = new Parent();
+		obj1.show();
+
+		// If a Parent type reference refers
+		// to a Child object Child's show()
+		// is called. This is called RUN TIME
+		// POLYMORPHISM.
+		Parent obj2 = new Child();
+		obj2.show();
+	}
+}
