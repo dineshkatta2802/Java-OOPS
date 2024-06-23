9d29a64c71db62f276ded58e75af707a5646f3bc
Dinesh Katta <kattadinesh2@gmail.com>
2024-06-23 06:28:50 +0530

    Explained Super Keyword in detailed way along  with examples

---
+++ Inheritance/Super_Keyword/Calling_The_Constructor.java
@@ -0,0 +1,21 @@
+package Inheritance.Super_Keyword;
+class Parent {
+    //Parent Class Constructor
+    Parent(){
+        System.out.println("Inside the Parent Class Consttructor");
+    }
+}
+
+class child extends Parent{
+    //Child Class Constructor
+    child(){
+        //Super Keyword to call the Parent class constructor
+        super();
+        System.out.println("Inside the Child Class Constructor");
+    }
+}
+public class Calling_The_Constructor {
+    public static void main(String[] args) {
+        child Dinesh = new child();
+    }
+}
---
+++ Inheritance/Super_Keyword/Calling_The_Fields.java
@@ -0,0 +1,22 @@
+package Inheritance.Super_Keyword;
+
+class Parent {
+    int x = 10;
+}
+
+class Child extends Parent {
+    int x = 20;
+
+    void display() {
+        System.out.println("Child class field: " + x);
+        System.out.println("Parent class field: " + super.x);
+    }
+}
+
+public class Calling_The_Fields {
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.display();
+    }
+}
+
