0f70b2100e2541e2cc985bc9bd4f784296ffa3a1
Dinesh Katta <kattadinesh2@gmail.com>
2024-06-23 13:48:07 +0530

    Updated

--- Polymorphism/Runtime_Polymorphism.java
+++ Polymorphism/Runtime_Polymorphism.java
@@ -2,20 +2,25 @@
 //Method overriding is one of the ways by which Java achieves ⁡⁣⁢⁣Run Time Polymorphism⁡⁣⁢⁣(Dynamic Polymorphism)⁡⁡. 
 //Writing 2 or more methods in super and sub class such that the methods have same name and parameters.
 
-
 package Polymorphism;
 // Base Class
 class Parent {
-	void show() { System.out.println("Parent's show()"); }
+	void show() {
+		System.out.println("Parent's show()"); 
+	}
 }
 
 // Inherited class
 class Child extends Parent {
 	// This method overrides show() of Parent
-	@Override void show()    //⁡⁣⁣⁢--->Note:⁡
-                             //@Override annotation is to inform the compiler and the 
-                             //reader that the method is intended to override a method 
-                             //in its superclass (Parent in this case)
+	@Override 
+	//⁡⁣⁣⁢--->Note:⁡
+    //@Override annotation is a useful tool for ensuring that methods are 
+    //correctly overridden, improving code clarity, and providing compile-time checks to prevent errors.
+	//@Override annotation is to inform the compiler and the 
+    //reader that the method is intended to override a method 
+    //in its superclass (Parent in this case)
+	void show()
 	{
 		System.out.println("Child's show()");
 	}
